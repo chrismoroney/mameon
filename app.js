43fc4586d53a79ead54ed044b5e047c444b28dd9
Christopher Moroney <christopher.moroney1@gmail.com>
2021-06-25 15:55:04 -0700

    many updates

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+
 
 var homeRouter = require('./routes/home');
 var servicesRouter = require('./routes/services');
@@ -21,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use('/', homeRouter);
 app.use('/services', servicesRouter);
